7607170caf894e14251d67a9de7f3d70f9cb9e98
deletefromuser <45934893+deletefromuser@users.noreply.github.com>
2023-07-27 09:59:53 +0800

    how-do-i-make-a-hibernation-batch-file-Windows 10

---
+++ bat/win10Hibernate.bat
@@ -0,0 +1,4 @@
+:: https://stackoverflow.com/questions/7977743/how-do-i-make-a-hibernation-batch-file
+:: https://answers.microsoft.com/en-us/windows/forum/all/anyone-know-a-batchscriptprogramexe-that-can-put/741c42d6-41a4-47b2-9c64-fddbf1605637
+cd c:\
+shutdown /h
